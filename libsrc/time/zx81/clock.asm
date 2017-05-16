f3226e512551abc4174a00a7faa2f1c6d546f90c
Andrew Dansby <andydansby@gmail.com>
2017-05-16 13:35:24 -0400

    Add files via upload

---
+++ libsrc/time/zx81/clock.asm
@@ -0,0 +1,19 @@
+;
+;	ZX81 clock() function
+;	By Stefano Bodrato - Oct. 2011
+;	Back to default FRAMES counter + 1 extra byte ;)
+;
+; --------
+; $Id: clock.asm,v 1.6 2011/11/02 14:24:33 stefano Exp $
+
+
+	XLIB	clock
+;	XREF	frames3
+
+.clock
+	ld	hl,-1
+	ld	de,($4034)
+	and a
+	sbc hl,de
+	ld	de,0
+	ret
