00050370d367d9712b747e431e0d5929177da033
Andrew Dansby <andydansby@gmail.com>
2017-05-16 06:55:07 -0400

    Add files via upload

---
+++ libsrc/zx81/clg.asm
@@ -0,0 +1,20 @@
+
+	XLIB	clg
+
+;
+;	$Id: clg.asm,v 1.3 2009/08/20 05:59:11 stefano Exp $
+;
+
+; ******************************************************************
+;
+;	Clear graphics area, 
+;
+
+		LIB	filltxt
+
+.clg
+		ld	l,0
+		jp	filltxt
+		
+		;call	restore81
+		;jp	2602	
---
+++ libsrc/zx81/clsgraph.asm
@@ -0,0 +1,22 @@
+
+	XLIB	cleargraphics
+
+;
+;	$Id: clsgraph.asm,v 1.4 2009/08/20 05:59:11 stefano Exp $
+;
+
+; ******************************************************************
+;
+;	Clear graphics area
+;
+
+		LIB	filltxt
+
+.cleargraphics
+		ld	l,0
+		jp	filltxt
+		
+		;call	restore81
+		;jp	2602	
+
+
