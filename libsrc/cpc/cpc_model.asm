0466f97aacaf94bfa29e19948d63578a4272a8e9
Andrew Dansby <andydansby@gmail.com>
2017-05-16 06:16:56 -0400

    Add files via upload

---
+++ libsrc/cpc/cpc_model.asm
@@ -0,0 +1,28 @@
+;
+; int cpc_model();
+; 
+; Results:
+;    0 - 464
+;    1 - 664
+;    2 - 6128
+
+
+; $Id: cpc_model.asm,v 1.5 2012/04/05 09:17:33 stefano Exp $
+
+
+        INCLUDE "cpcfirm.def"              
+
+        XDEF cpc_model
+
+.cpc_model
+	call	firmware
+	defw	kl_probe_rom	; 0B915H
+	ld	a,h		; version
+	ld	hl,1
+	rra
+	ret	c	; 664
+	ld	l,2
+	rra
+	ret	c	; 6128
+	ld	l,h
+	ret		; 464
---
+++ libsrc/cpc/firmware.asm
@@ -0,0 +1,35 @@
+;
+; Amstrad CPC Specific libraries
+;
+; Stefano Bodrato - May 2008
+;
+;
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;
+; Firmware interposer
+;
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;
+; Used internally only
+;
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;
+;
+; $Id: firmware.asm,v 1.2 2008/05/26 06:38:08 stefano Exp $
+;
+
+        XLIB firmware
+        XREF firmware_bc
+        
+.firmware
+        exx                      ; Use alternate registers
+        ex      (sp),hl          ; get return address
+        ld      c,(hl)
+        inc     hl
+        ld      b,(hl)           ; BC=BASIC address
+        inc     hl
+        ex      (sp),hl          ; restore return address
+        push    bc               ; firmware location to jump to first
+        ld      bc,(firmware_bc)
+        exx                      ; Back to the regular set
+        ret                      ; And call the firmware routine
